a20148685fde06a4290170c86d218f8d68d8d17a
abebed19 <abebed19@gmail.com>
2026-02-23 02:40:51 +0300

    javas default equals method and its difference

---
+++ Notepad.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aaron
+ */
+public class Notepad {
+        private String name;
+        private int year;
+ 
+    public Notepad(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public boolean equals(Object object) {
+        if (object == null || this.getClass() != object.getClass()) {
+            return false;
+        }
+
+        if (object == this) {
+            return true;
+        }
+
+        Notepad compared = (Notepad) object;
+
+        return this.name.equals(compared);
+    }
+    public static void main(String ... args){
+        Notepad basics = new Notepad("Equals basics", 2000);
+        Notepad advanced = new Notepad("Equals advanced", 2001);
+
+        System.out.println(basics.equals(basics));
+        System.out.println(basics.equals(advanced));
+        System.out.println(basics.equals(new Notepad("Equals basics", 2000)));
+        System.out.println(basics.equals(new Notepad("Equals basics", 2001)));
+    }
+    
+}
